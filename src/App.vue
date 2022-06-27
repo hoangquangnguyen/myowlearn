a78f15e9c6d53d61cadb9ad4c07a9188aad0d4e7
nhquang192 <nhquang192@gmail.com>
2022-06-27 09:14:01 +0700

    init project + add component HeroCom

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <HeroCom />
+  <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+
+  <!-- <router-view /> -->
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeroCom from './components/HeroCom.vue'
 
+export default defineComponent({
+  components: {
+    HeroCom,
+  },
+})
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;700;800&display=swap');
 </style>
---
+++ src/components/HeroCom.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex items-center justify-between px-[100px] py-[36px]">
+    <a href="/" class="flex flex-row items-center">
+      <img src="@/assets/logo.png" alt="" class="h-11 w-20" />
+      <span class="text-3xl text-[#313273]">Owlearn</span>
+    </a>
+    <div
+      class="text-base font-normal flex items-center justify-between text-center flex-1 ml-[60px]"
+    >
+      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
+        Home
+      </div>
+      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
+        Bootcamp
+      </div>
+      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
+        Events
+      </div>
+      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
+        Article
+      </div>
+      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
+        Contact Us
+      </div>
+    </div>
+    <button
+      class="ml-[60px] px-10 py-4 border-[1px] rounded-lg font-bold text-base text-[#DC4298] border-[#DC4298]"
+    >
+      Contact Us
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HeroCom',
+})
+</script>
+
+<style scoped></style>
