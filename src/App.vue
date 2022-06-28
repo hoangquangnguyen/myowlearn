7ea085762829e07e22517488226b2c2a6fb86c0a
nhquang192 <nhquang192@gmail.com>
2022-06-28 09:42:57 +0700

    add Testimonial component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <HeroCom />
   <PartnersCom />
   <AboutUsCom />
+  <CourseCom />
+  <CtaCom />
+  <GraduatedCom />
+  <TestimonialCom />
+  <FooterCom />
   <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -14,12 +19,22 @@ import { defineComponent } from 'vue'
 import HeroCom from './components/HeroCom.vue'
 import PartnersCom from './components/PartnersCom.vue'
 import AboutUsCom from './components/AboutUsCom.vue'
+import CourseCom from './components/CourseCom.vue'
+import CtaCom from './components/CtaCom.vue'
+import GraduatedCom from './components/GraduatedCom.vue'
+import TestimonialCom from './components/TestimonialCom.vue'
+import FooterCom from './components/FooterCom.vue'
 
 export default defineComponent({
   components: {
     HeroCom,
     PartnersCom,
     AboutUsCom,
+    CourseCom,
+    CtaCom,
+    GraduatedCom,
+    TestimonialCom,
+    FooterCom,
   },
 })
 </script>
---
+++ src/components/TestimonialCom.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-row flex-1 items-center justify-between mb-20">
+    <div class="rounded-md h-[420px]">
+      <img src="@/assets/testimonial-img.png" alt="" />
+    </div>
+    <div class="flex flex-col flex-1 items-start">
+      <h3 class="font-bold text-4xl mb-10">What did they say</h3>
+      <p class="text-xl font-normal">
+        Higher education in the era of the industrial revolution 4.0 requires
+        breakthrough learning using digital platforms that answer the challenges
+        of millennial students to study anywhere, anytime and with leading-edge
+        ICT technology. From student recruitment to teaching and learning
+        administration processes,
+      </p>
+      <hr class="my-10 bg-black w-full" />
+      <div class="w-full flex flex-row items-center justify-between">
+        <div class="flex flex-col items-center">
+          <span class="font-semibold text-2xl">Briana Patton</span>
+          <span>Designer at Salesforce</span>
+        </div>
+
+        <button class="rounded-full p-1">
+          <img src="@/assets/arrow-right-circle.png" alt="" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestimonialCom',
+})
+</script>
