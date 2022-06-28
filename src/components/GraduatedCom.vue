1dd7c5182554b0a14d0df8f6226ad69562df45aa
nhquang192 <nhquang192@gmail.com>
2022-06-28 09:17:38 +0700

    add Graduated component

---
+++ src/components/GraduatedCom.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col items-center mb-20">
+    <h4 class="mb-11 font-semibold text-3xl">Our graduates have worked in</h4>
+    <div class="grid grid-cols-5 items-center justify-between gap-x-16 gap-y-8">
+      <img src="@/assets/clickup-logo.png" alt="" />
+      <img src="@/assets/dropbox-blue.png" alt="" />
+      <img src="@/assets/elastic-logo.png" alt="" />
+      <img src="@/assets/github-logo.png" alt="" />
+      <img src="@/assets/freshbooks-logo.png" alt="" />
+      <img src="@/assets/helpscout-logo.png" alt="" />
+      <img src="@/assets/hubspot-logo.png" alt="" />
+      <img src="@/assets/intuit-logo.png" alt="" />
+      <img src="@/assets/google-logo.png" alt="" />
+      <img src="@/assets/paychex-logo.png" alt="" />
+      <img src="@/assets/salesforce-logo.png" alt="" />
+      <img src="@/assets/sap-logo.png" alt="" />
+      <img src="@/assets/twiliosegment-logo.png" alt="" />
+      <img src="@/assets/servicenow-logo.png" alt="" />
+      <img src="@/assets/shoptify-logo.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'GraduatedCom',
+})
+</script>
