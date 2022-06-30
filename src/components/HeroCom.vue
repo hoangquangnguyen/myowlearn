58cd1bb1f9755a2038b6f314823e22607bbc53a7
nhquang192 <nhquang192@gmail.com>
2022-06-30 14:53:42 +0700

    add NavBar

--- src/components/HeroCom.vue
+++ src/components/HeroCom.vue
@@ -1,37 +1,4 @@
 <template>
-  <!-- Hearder -->
-  <div
-    class="mydiv flex items-center justify-between py-[36px] mb-[60px] text-brown2"
-  >
-    <a href="/" class="flex flex-row items-center">
-      <img src="@/assets/img/logo.png" alt="" class="h-11 w-20" />
-      <span class="text-3xl text-[#313273]">Owlearn</span>
-    </a>
-    <div
-      class="text-base font-normal flex items-center justify-between text-center flex-1 ml-[60px]"
-    >
-      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
-        Home
-      </div>
-      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
-        Bootcamp
-      </div>
-      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
-        Events
-      </div>
-      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
-        Article
-      </div>
-      <div class="flex-1 cursor-pointer hover:underline hover:font-bold">
-        Contact Us
-      </div>
-    </div>
-    <button
-      class="ml-[60px] px-10 py-4 border-[1px] rounded-lg font-bold text-base text-pinkD border-pinkD hover:bg-pink-100 transition ease-out duration-300"
-    >
-      Contact Us
-    </button>
-  </div>
   <!-- Banner -->
   <div class="mydiv flex flex-row items-center justify-between">
     <div class="flex-1">
